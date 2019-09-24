e9d3f87daa7ed331f617711622ac94b03dae4f50
tomo0314 <you@example.com>
2019-09-24 18:52:49 +0900

    index

--- App.js
+++ App.js
@@ -4,7 +4,42 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.appbar}>
+        <View>
+          <Text style={styles.appbarTitle}>MEMOT</Text>
+        </View>
+      </View>
+
+      <View style={styles.memoList}>  
+        <View style={styles.memoListItem}>  
+          <Text　style={styles.memoTitle}>samples</Text>
+          <Text　style={styles.memodata}>2019/09/23</Text>
+        </View>
+
+        <View style={styles.memoListItem}>  
+          <Text　style={styles.memoTitle}>samples</Text>
+          <Text　style={styles.memodata}>2019/09/23</Text>
+        </View>
+
+        <View style={styles.memoListItem}>  
+          <Text　style={styles.memoTitle}>samples</Text>
+          <Text　style={styles.memodata}>2019/09/23</Text>
+        </View>
+
+        <View style={styles.memoListItem}>  
+          <Text　style={styles.memoTitle}>samples</Text>
+          <Text　style={styles.memodata}>2019/09/23</Text>
+        </View>
+
+        <View style={styles.memoListItem}>  
+          <Text　style={styles.memoTitle}>samples</Text>
+          <Text　style={styles.memodata}>2019/09/23</Text>
+        </View>
+      </View>
+
+      <View style={styles.memoAddButton}>  
+        <Text style={styles.memoAddButtonTitle}>+</Text>
+      </View>
     </View>
   );
 }
@@ -12,8 +47,78 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FFFDF6',
     alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: 78,
   },
+
+  appbar: {
+    position: 'absolute',
+    top: 0,
+    height:78,
+    left: 0,
+    right: 0,
+    paddingTop: 30,
+    backgroundColor: '#265366',
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {widdth: 0, height: 0 },
+    shadowOpacity: 1,
+    shadowRadius: 3,
+    zIndex: 10,
+  },
+
+  appbarTitle: {
+    color: '#fff',
+    fontSize: 18,
+  },
+
+  memoList: {
+    width: '100%',
+    flex: 1,
+  },
+ 
+  memoListItem: {
+    padding: 16,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ddd',
+    backgroundColor: '#fff',
+  },
+
+  memoTitle: {
+    fontSize: 18,
+    marginBottom: 4,
+  },
+
+  memoDate: {
+    fontSize: 12,
+    color: '#a2a2a2',
+  },
+
+  memoAddButton: {
+    position: 'absolute',
+    bottom: 32,
+    right: 32,
+    width: 48,
+    height: 48,
+    backgroundColor: '#E31676',
+    borderRadius: 24,
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {widdth: 0, height: 2 },
+    shadowOpacity: 0.5,
+    shadowRadius: 3,
+
+  },
+
+  memoAddButtonTitle: {
+    fontSize: 32,
+    lineHeight: 32,
+    color: '#fff',
+  },
+
+
 });
