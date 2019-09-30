7d59c0ba47ee02bb4fb8ab03103ba690c2f3199a
tomo0314 <you@example.com>
2019-09-30 22:40:00 +0900

    Create Login and Signup Screen

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import Appbar from './src/components/Appbar';
 import MemoListScreen from './src/screens/MemoListScreen'
 import MemoDetailScreen from './src/screens/MemoDetailScreen'
 import MemoEditScreen from './src/screens/MemoEditScreen'
-
+import LoginScreen from './src/screens/LoginScreen'
+import SignupScreen from './src/screens/SignupScreen'
 
 export default function App() {
   return (
@@ -12,7 +13,9 @@ export default function App() {
     <Appbar />
     {/* <MemoListScreen /> */}
     {/* <MemoDetailScreen /> */}
-    <MemoEditScreen />
+    {/* <MemoEditScreen /> */}
+    {/* <LoginScreen /> */}
+    <SignupScreen />
     </View>
   );
 }
